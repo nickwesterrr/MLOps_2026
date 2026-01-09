96f834d1f9da7a65d2679531f9df927a01258d28
Nick Wester <nick9@wester>
2026-01-09 20:46:25 +0100

    implemented the MLP

--- src/ml_core/models/mlp.py
+++ src/ml_core/models/mlp.py
@@ -17,9 +17,24 @@ class MLP(nn.Module):
         # TODO: Build the MLP architecture
         # If you are up to the task, explore other architectures or model types
         # Hint: Flatten -> [Linear -> ReLU -> Dropout] * N_layers -> Linear
+        input_dim = 1
+        for d in input_shape:
+            input_dim *= d
         
-        pass
+        hidden_dim1, hidden_dim2 = hidden_units
+
+        self.net = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(input_dim, hidden_dim1),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_dim1, hidden_dim2),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_dim2, num_classes),
+        )
+        
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # TODO: Implement forward pass
-        pass
+        return self.net(x)
